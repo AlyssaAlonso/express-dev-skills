b7be4eaf770660d753e049f6ebd199774b44daf8
Alyssa Alonso <aalonso310@gmail.com>
2023-10-18 22:15:05 -0400

    add tasks from part two of the lab

--- controllers/skills.js
+++ controllers/skills.js
@@ -3,7 +3,22 @@ const Skill = require("../models/skill");
 module.exports = {
   index,
   show,
+  new: newSkill,
+  create,
+  delete: deleteSkill,
 };
+
+function create(req, res) {
+  console.log(req.body);
+  // The model is responsible for creating data
+  Skill.create(req.body);
+  // Do a redirect anytime data is changed
+  res.redirect("/skills");
+}
+
+function newSkill(req, res) {
+  res.render("skills/new", { title: "New Todo" });
+}
 
 function show(req, res) {
   res.render("skills/show", {
@@ -12,7 +27,13 @@ function show(req, res) {
 }
 
 function index(req, res) {
+  console.log(req.query);
   res.render("skills/index", {
     skills: Skill.getAll(),
   });
+}
+
+function deleteSkill(req, res) {
+  Skill.deleteOne(req.params.id);
+  res.redirect("/skills");
 }
